0afdd3b2a6eec434bb9445ca2fd9f12bd49b219b
Tac Tacelosky <tacman@gmail.com>
2022-05-29 07:50:37 -0400

    use AbstractBundle

--- src/SurvosWorkflowBundle.php
+++ src/SurvosWorkflowBundle.php
@@ -1,8 +1,10 @@
 <?php
 namespace Survos\WorkflowBundle;
 
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-class SurvosWorkflowBundle extends Bundle
+
+class SurvosWorkflowBundle extends AbstractBundle
 {
 
 }
