30261ab8db2eabfa8e65d5f5cd9e34806b56712a
Tac Tacelosky <tacman@gmail.com>
2022-09-13 20:02:34 -0500

    fix jsonResponseTrait

--- src/Traits/WorkflowHelperTrait.php
+++ src/Traits/WorkflowHelperTrait.php
@@ -4,6 +4,7 @@
 namespace Survos\WorkflowBundle\Traits;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Survos\CoreBundle\Traits\JsonResponseTrait;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Survos\WorkflowBundle\Traits\MarkingInterface;
 
 trait WorkflowHelperTrait
 {
-    use JsonResponseTrai;
+    use JsonResponseTrait;
 
     protected function _transition(Request $request, MarkingInterface $entity, $transition, WorkflowInterface $stateMachine, EntityManagerInterface $entityManager, $class, $_format = 'json'): Response
     {
