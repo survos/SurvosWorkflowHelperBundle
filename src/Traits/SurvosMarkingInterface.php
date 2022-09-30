ccbd5f0f263fe357a2c715ef96cdec7373bad4b4
tacman <info@survos-ci.com>
2022-09-30 09:49:49 +0000

    prepare release

--- src/Traits/SurvosMarkingInterface.php
+++ src/Traits/SurvosMarkingInterface.php
@@ -5,7 +5,6 @@ namespace Survos\WorkflowBundle\Traits;
 interface SurvosMarkingInterface
 {
     public function getMarking(): ?string;
-    public function setMarking(?string $marking, $context=[]): self;
 
-
+    public function setMarking(?string $marking, $context = []): self;
 }
--- src/Traits/TransitionsBuilderTrait.php
+++ src/Traits/TransitionsBuilderTrait.php
@@ -1,10 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 
 namespace Survos\WorkflowBundle\Traits;
 
-use Survos\BaseBundle\Entity\SurvosBaseEntity;
+use Survos\BootstrapBundle\Entity\SurvosBaseEntity;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Workflow\Transition;
@@ -12,30 +13,29 @@ use Symfony\Component\Workflow\WorkflowInterface;
 
 trait TransitionsBuilderTrait
 {
-
     public function addTransitions(WorkflowInterface $workflow, SurvosBaseEntity $entity, FormBuilderInterface $builder): void
     {
         $metadataStore = $workflow->getMetadataStore();
         /** @var Transition $transition */
         foreach ($workflow->getEnabledTransitions($entity) as $transition) {
-
-
             // could add the colors to the transitions in extra!
             $transitionMetadata = $metadataStore->getTransitionMetadata($transition);
             $builder
                 ->add($transition->getName(), SubmitType::class, [
                     'attr' => [
                         'style' => 'float: left',
-                        'class' => sprintf("btn btn-sm float-left transition transition-%s %s",
+                        'class' => sprintf(
+                            "btn btn-sm float-left transition transition-%s %s",
                             $transition->getName(),
-                            $metadataStore->getTransitionMetadata($transition)['class'] ?? '')]
+                            $metadataStore->getTransitionMetadata($transition)['class'] ?? ''
+                        ),
+                    ],
                 ]);
             if (($transitionMetadata['frequency'] ?? 'low') <> 'low') {
             }
         }
         try {
         } catch (\Exception $e) {
-
         }
 
         // really we should be checking if new or not, can't save w/o transition if new.  Or maybe we can.  Hmm.
@@ -43,11 +43,10 @@ trait TransitionsBuilderTrait
                 'label' => "Update (no transition)",
                 'attr' => [
                     'style' => 'float: right',
-                    'class' => "btn btn-sm btn-primary float-right"
-                ]
+                    'class' => "btn btn-sm btn-primary float-right",
+                ],
             ]);
         if ($entity->getId()) {
         }
     }
-
 }
