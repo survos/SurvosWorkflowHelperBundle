479a0ec1818ce487d73b374664007e39d90b03f8
tacman <info@survos-ci.com>
2023-03-19 11:06:15 +0000

    fix JsonSearch to handle searchPanes

--- src/SurvosWorkflowBundle.php
+++ src/SurvosWorkflowBundle.php
@@ -52,10 +52,9 @@ class SurvosWorkflowBundle extends AbstractBundle implements CompilerPassInterfa
 
         $config = (new Processor())->processConfiguration($configuration, $configs);
         $container->setParameter('workflows.configuration', $workflowConfig = $config['workflows']['workflows'] ?? []);
-        $transitionListenerDefintion = $container->findDefinition(TransitionListener::class);
-
-        $transitionListenerDefintion->setArgument('$workflowHelperService', new Reference(WorkflowHelperService::class));
-        $transitionListenerDefintion->setArgument('$workflows', tagged_iterator('workflow'));
+//        $transitionListenerDefintion = $container->findDefinition(TransitionListener::class);
+//        $transitionListenerDefintion->setArgument('$workflowHelperService', new Reference(WorkflowHelperService::class));
+//        $transitionListenerDefintion->setArgument('$workflows', tagged_iterator('workflow'));
 
         $workflowHelperDefinition = $container->findDefinition(WorkflowHelperService::class);
         $container->findDefinition(SurvosWorkflowDumpCommand::class)
