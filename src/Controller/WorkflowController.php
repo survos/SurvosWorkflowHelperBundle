3e8fdccd77f6639b8dbb49320e5e836055faa157
tacman <info@survos-ci.com>
2024-01-21 16:30:43 +0000

    use Attribute route, etc.

--- src/Controller/WorkflowController.php
+++ src/Controller/WorkflowController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Workflow\Workflow;
 
 class WorkflowController extends AbstractController
